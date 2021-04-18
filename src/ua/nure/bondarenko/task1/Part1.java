9c004749b9f81b3bc7f9f186b1e6a9ec8d6adf2a
vovan762000@gmail.com <vovan762000@gmail.com>
2021-04-18 18:52:44 +0300

    solve task 7

--- src/ua/nure/bondarenko/task1/Part1.java
+++ src/ua/nure/bondarenko/task1/Part1.java
@@ -5,14 +5,9 @@ import static ua.nure.bondarenko.task1.Util.*;
 
 public class Part1 {
     public static int getSum(String[] array) {
-        try {
             return Arrays.stream(array)
                     .mapToInt(Integer::parseInt)
                     .sum();
-        } catch (NumberFormatException e) {
-            System.out.println("Input correct sequence");
-        }
-        return 0;
     }
 
     public static void main(String[] args) {
---
+++ src/ua/nure/bondarenko/task1/Part7.java
@@ -0,0 +1,40 @@
+package ua.nure.bondarenko.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ua.nure.bondarenko.task1.Util.checkInputLikeDigit;
+
+public class Part7 {
+
+    public static void getSimpleDigitsWithNaxSum(String[] array) {
+        List<String> simpleList = new ArrayList<>();
+        int numberOfPrimeDigits = Integer.parseInt(array[0]);
+        int nextNumber = 2;
+        while (simpleList.size() < numberOfPrimeDigits) {
+            if (Part5.isPrime(nextNumber)) {
+                simpleList.add(String.valueOf(nextNumber));
+            }
+            nextNumber++;
+        }
+        int maxSum = 0;
+        int tmp = 0;
+        int count = 1;
+        for (String primeNumber : simpleList) {
+            tmp = Part1.getSum(primeNumber.split(""));
+            if (tmp > maxSum) {
+                maxSum = tmp;
+                count = 1;
+            } else if (tmp == maxSum) {
+                count++;
+            }
+        }
+        System.out.println(maxSum + " " + count);
+    }
+
+    public static void main(String[] args) {
+        if (checkInputLikeDigit(args)) {
+            getSimpleDigitsWithNaxSum(args);
+        }
+    }
+}
